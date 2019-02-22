cd760c4db96a282b294f4f1fe5b074bc35f0cf98
loveemu <loveemu@gmail.com>
2019-02-23 00:12:40 +0900

    Add version detection and improve scanning accuracy

--- tools/gax_scanner/gax_scanner.py
+++ tools/gax_scanner/gax_scanner.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import re
 import struct
 
 
@@ -30,47 +31,78 @@ def parse_song_info(rom, end_offset):
 
     while rom[start_offset] != ord('"'):
         start_offset += 1
+    while rom[start_offset + 1] == ord('"'):
+        start_offset += 1
 
     return rom[start_offset:end_offset].decode("iso-8859-1")
 
 
-def parse_gax_music_header(rom, offset):
-    # Only GAX ENGINE V3 is supported
-    if offset + 200 > len(rom):
+def parse_gax_version(rom, offset):
+    if offset + 17 >= len(rom):
+        return None
+    if rom[offset:offset + 17] != b'GAX Sound Engine ':
         return None
 
-    num_tracks = struct.unpack_from("<H", rom, offset)[0]
-    max_tracks = 32  # the number is randomly determined
-    if num_tracks == 0 or num_tracks > max_tracks:
+    end_offset = offset + 17
+    while rom[end_offset:end_offset + 1] != b' ':
+        end_offset += 1
+        if end_offset >= len(rom):
+            return None
+
+    version = {"text": rom[offset + 17:end_offset].decode()}
+    m = re.fullmatch(r"(\d)\.(\d{1,3})([A-Za-z]?)", version["text"])
+    if m:
+        version["major_version"] = int(m.group(1))
+        version["minor_version"] = int(m.group(2))
+        version["revision"] = m.group(3)
+    return version
+
+
+def parse_gax_music_header_v3(rom, offset):
+    if offset + 0x20 >= len(rom):
+        return None
+
+    fields = struct.unpack_from("<HHHHHHLLLHHHH", rom, offset)
+    num_channels = fields[0]
+    max_channels = 32  # according to the official GAX page
+    if num_channels == 0 or num_channels > max_channels:
         return None
 
     header = {
-        "num_tracks": num_tracks,
-        "pattern_length": struct.unpack_from("<H", rom, offset + 2)[0],
-        "num_patterns": struct.unpack_from("<H", rom, offset + 4)[0],
-        "seq_address": struct.unpack_from("<L", rom, offset + 0xc)[0],
-        "instr_address": struct.unpack_from("<L", rom, offset + 0x10)[0],
-        "sample_address": struct.unpack_from("<L", rom, offset + 0x14)[0],
+        "num_channels": num_channels,
+        "pattern_length": fields[1],
+        "num_patterns": fields[2],
+        "master_volume": fields[4],
+        "seq_address": fields[6],
+        "instr_address": fields[7],
+        "sample_address": fields[8],
     }
 
-    if not is_rom_address(header["seq_address"]) or to_offset(header["seq_address"]) > len(rom):
-        return None
-    if not is_rom_address(header["instr_address"]) or to_offset(header["instr_address"]) > len(rom):
-        return None
-    if not is_rom_address(header["sample_address"]) or to_offset(header["sample_address"]) > len(rom):
+    if fields[12] != 0:
         return None
 
-    header["tracks"] = []
-    for addr in struct.unpack_from("<" + "L" * num_tracks, rom, offset + 0x20):
-        if not is_rom_address(addr):
+    if not is_rom_address(header["seq_address"]) or to_offset(header["seq_address"]) > len(rom) or header["seq_address"] % 4 != 0:
+        return None
+    if not is_rom_address(header["instr_address"]) or to_offset(header["instr_address"]) > len(rom) or header["instr_address"] % 4 != 0:
+        return None
+    if not is_rom_address(header["sample_address"]) or to_offset(header["sample_address"]) > len(rom) or header["sample_address"] % 4 != 0:
+        return None
+
+    if offset + 0x20 + (num_channels * 4) >= len(rom):
+        return None
+
+    channel_addresses = []
+    for addr in struct.unpack_from("<" + "L" * num_channels, rom, offset + 0x20):
+        if not is_rom_address(addr) or addr % 4 != 0:
             return None
-        header["tracks"].append({"address": addr})
+        channel_addresses.append(addr)
 
-    for addr in struct.unpack_from("<" + "L" * (max_tracks - num_tracks), rom, offset + 0x20 + num_tracks * 4):
-        if addr != 0:
-            return None
+    header["channels"] = []
+    for addr in channel_addresses:
+        header["channels"].append({"address": addr})
 
-    header["info"] = parse_song_info(rom, to_offset(header["tracks"][0]["address"]))
+    # channel address list is not sorted sometimes (test case: Finding Nemo)
+    header["info"] = parse_song_info(rom, to_offset(sorted(channel_addresses)[0]))
     return header
 
 
@@ -79,16 +111,28 @@ def gax_scan(filename):
     if size < 0xc0 or size > 0x2000000:
         raise ValueError("Input too small/large")
 
-    gax = {"music": {}}
+    gax = None
     with open(filename, "rb") as f:
         rom = f.read()
 
         for offset in range(0, len(rom), 4):
-            song_header = parse_gax_music_header(rom, offset)
+            version = parse_gax_version(rom, offset)
+            if version:
+                gax = {"version": version, "music": {}}
+                break
+
+        if not gax:
+            return None
+
+        if version["major_version"] != 3:
+            return gax
+
+        for offset in range(0, len(rom), 4):
+            song_header = parse_gax_music_header_v3(rom, offset)
             if song_header:
                 gax["music"][to_address(offset)] = song_header
 
-    return gax if len(gax["music"]) > 0 else None
+    return gax
 
 
 def main():
@@ -98,6 +142,7 @@ def main():
 
     gax = gax_scan(args.filename)
     if gax:
+        print("GAX Sound Engine " + gax["version"]["text"])
         print("%d songs" % len(gax["music"]))
         for address, header in gax["music"].items():
             print("%08X %s" % (address, header["info"]))
